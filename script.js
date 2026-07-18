c3fcc6bf6870325354a24876d80bc77524a42ff6
BladestormGames <bladestormgames@gmail.com>
2026-07-18 00:21:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,23 @@ fetch("data/rings.json")
     .then(response => response.json())
     .then(rings => {
 
-        console.log(rings);
+        rings.forEach(ring => {
+
+            ringList.innerHTML += `
+                <div class="ring">
+
+                    <div class="ring-icon">
+                        <img src="images/${ring.icon}" alt="${ring.name}">
+                    </div>
+
+                    <div class="ring-info">
+                        <div class="ring-name">${ring.name}</div>
+                        <div class="ring-desc">${ring.desc}</div>
+                    </div>
+
+                </div>
+            `;
+
+        });
 
     });
