c3335d80bccd13275a5f43a4052b24c7faf09ff1
BladestormGames <bladestormgames@gmail.com>
2026-07-18 00:33:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
 const ringList = document.getElementById("ring-list");
+const searchBox = document.getElementById("search");
 
+let allRings = [];
+
+// Draws the rings onto the page
+function displayRings(rings)
+{
+    ringList.innerHTML = "";
+
+    rings.forEach(ring =>
+    {
+        ringList.innerHTML += `
+            <div class="ring">
+
+                <div class="ring-icon">
+                    <img src="images/rings/${ring.icon}" alt="${ring.name}">
+                </div>
+
+                <div class="ring-info">
+                    <div class="ring-name">${ring.name}</div>
+                    <div class="ring-desc">${ring.desc}</div>
+                </div>
+
+            </div>
+        `;
+    });
+}
+
+// Load the JSON
 fetch("data/rings.json")
     .then(response => response.json())
-    .then(rings => {
+    .then(rings =>
+    {
+        allRings = rings;
 
-        rings.forEach(ring => {
-
-            ringList.innerHTML += `
-                <div class="ring">
-
-                    <div class="ring-icon">
-                        <img src="images/rings/${ring.icon}" alt="${ring.name}">
-                    </div>
-
-                    <div class="ring-info">
-                        <div class="ring-name">${ring.name}</div>
-                        <div class="ring-desc">${ring.desc}</div>
-                    </div>
-
-                </div>
-            `;
-
-        });
-
+        displayRings(allRings);
     });
+
+// Search
+searchBox.addEventListener("input", function()
+{
+    const search = searchBox.value.toLowerCase();
+
+    const filtered = allRings.filter(ring =>
+    {
+        return ring.name.toLowerCase().includes(search);
+    });
+
+    displayRings(filtered);
+});
